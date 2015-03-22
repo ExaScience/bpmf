a0c6922e7a62eae447265f2e3834cecaa86894bb
Tom Vander Aa <vanderaa@imec.be>
2015-03-22 15:50:13 +0100

    Added mvnormal distribution

---
+++ c++/mvnormal.cpp
@@ -0,0 +1,57 @@
+
+/*
+ * From:
+ * http://stackoverflow.com/questions/6142576/sample-from-multivariate-normal-gaussian-distribution-in-c
+ */
+
+#include <Eigen/Dense>
+#include <boost/random/mersenne_twister.hpp>
+#include <boost/random/normal_distribution.hpp>    
+
+/*
+  We need a functor that can pretend it's const,
+  but to be a good random number generator 
+  it needs mutable state.
+*/
+namespace Eigen {
+namespace internal {
+template<typename Scalar> 
+struct scalar_normal_dist_op 
+{
+  static boost::mt19937 rng;    // The uniform pseudo-random algorithm
+  mutable boost::normal_distribution<Scalar> norm;  // The gaussian combinator
+
+  EIGEN_EMPTY_STRUCT_CTOR(scalar_normal_dist_op)
+
+  template<typename Index>
+  inline const Scalar operator() (Index, Index = 0) const { return norm(rng); }
+};
+
+template<typename Scalar> boost::mt19937 scalar_normal_dist_op<Scalar>::rng;
+
+template<typename Scalar>
+struct functor_traits<scalar_normal_dist_op<Scalar> >
+{ enum { Cost = 50 * NumTraits<Scalar>::MulCost, PacketAccess = false, IsRepeatable = false }; };
+} // end namespace internal
+} // end namespace Eigen
+
+/*
+  Draw nn samples from a size-dimensional normal distribution
+  with a specified mean and covariance
+*/
+Eigen::MatrixXd MvNormal(Eigen::MatrixXd covar, Eigen::VectorXd mean, int nn = 1) 
+{
+  int size = mean.cols(); // Dimensionality (rows)
+  Eigen::internal::scalar_normal_dist_op<double> randN; // Gaussian functor
+  Eigen::internal::scalar_normal_dist_op<double>::rng.seed(1); // Seed the rng
+  Eigen::MatrixXd normTransform(size,size);
+
+  Eigen::LLT<Eigen::MatrixXd> cholSolver(covar);
+  normTransform = cholSolver.matrixL();
+
+  Eigen::MatrixXd samples = (normTransform 
+                           * Eigen::MatrixXd::NullaryExpr(size,nn,randN)).colwise() 
+                           + mean;
+
+  return samples;
+}
