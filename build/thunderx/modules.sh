84dea76424654f0a9a67f9f985a78bfcb8be6485
Tom Vander Aa <vanderaa@imec.be>
2018-10-19 09:12:46 +0200

    Working on thunderX benchmarking

--- build/thunderx/modules.sh
+++ build/thunderx/modules.sh
@@ -1,1 +1,7 @@
 module load THUNDER/ARM_PL/18.3_armhpc18.3 HPC_COMPILER/18.3 eigen 
+module load mpich/3.2
+module load arm_forge
+
+export ARM_HPC_COMPILER_LICENSE_SEARCH_PATH=$HOME/euroexa/arm/license
+export ALLINEA_LICENSE_DIR=$HOME/euroexa/arm/license
+export MPICC=armclang
