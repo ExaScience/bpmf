e63ce5f4ddd35889a589b105738d1ed88f500a5a
Tom Vander Aa <vanderaa@imec.be>
2020-08-14 14:22:26 +0200

    MPI_Allreduce using new datalayout for precMu/precLambda

--- c++/mpi_allreduce.h
+++ c++/mpi_allreduce.h
@@ -29,8 +29,8 @@ void MPI_Sys::sample(Sys &in)
 
         for (int i = 0; i < in.num(); i++)
         {
-            MPI_Allreduce(MPI_IN_PLACE, in.precMu.at(i).data(), num_latent, MPI_DOUBLE, MPI_SUM, MPI_COMM_WORLD);
-            MPI_Allreduce(MPI_IN_PLACE, in.precLambda.at(i).data(), num_latent*num_latent, MPI_DOUBLE, MPI_SUM, MPI_COMM_WORLD);
+            MPI_Allreduce(MPI_IN_PLACE, in.precMu.col(i).data(), num_latent, MPI_DOUBLE, MPI_SUM, MPI_COMM_WORLD);
+            MPI_Allreduce(MPI_IN_PLACE, in.precLambda.col(i).data(), num_latent*num_latent, MPI_DOUBLE, MPI_SUM, MPI_COMM_WORLD);
         }
         bcast_sum_cov_norm();
     }
