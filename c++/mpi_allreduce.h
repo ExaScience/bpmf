eb6818e03b461ddde84c2438e36fd750f13edbe9
Tom Vander Aa <vanderaa@imec.be>
2020-09-17 12:42:06 +0200

    Move reduce to beginning of sample()

--- c++/mpi_allreduce.h
+++ c++/mpi_allreduce.h
@@ -20,34 +20,37 @@ struct MPI_Sys : public Sys
 
 };
 
+
 void MPI_Sys::sample(Sys &in)
 {
-    { BPMF_COUNTER("compute"); Sys::sample(in); }
-
     {
         BPMF_COUNTER("communicate");
 
 
         for (int i = 0; i < nprocs; i++)
         {
-            auto col = in.from(i);
-            auto num_cols = in.num(i);
+            auto col = from(i);
+            auto num_cols = num(i);
 
             {
-                auto recv_buf = in.precMu.col(col).data();
+                auto recv_buf = precMu.col(col).data();
                 auto send_buf = (i == procid) ? MPI_IN_PLACE : recv_buf;
                 MPI_Reduce(send_buf, recv_buf, num_cols * num_latent, MPI_DOUBLE, MPI_SUM, i, MPI_COMM_WORLD);
             }
 
             {
-                auto recv_buf = in.precLambda.col(col).data();
+                auto recv_buf = precLambda.col(col).data();
                 auto send_buf = (i == procid) ? MPI_IN_PLACE : recv_buf;
                 MPI_Reduce(send_buf, recv_buf, num_cols * num_latent * num_latent, MPI_DOUBLE, MPI_SUM, i, MPI_COMM_WORLD);
             }
         }
 
-        reduce_sum_cov_norm();
     }
+
+    { BPMF_COUNTER("compute"); Sys::sample(in); }
+
+
+    reduce_sum_cov_norm();
 }
 
 #include "mpi_common.h"
