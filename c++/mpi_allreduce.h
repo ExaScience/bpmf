d1c95b8019db50beadc4d5f6d005d377dfb1f652
Tom Vander Aa <vanderaa@imec.be>
2021-03-30 17:08:50 +0200

    WIP: reduce into new block

--- c++/mpi_allreduce.h
+++ c++/mpi_allreduce.h
@@ -26,6 +26,45 @@ struct AllReduceBlock
     Eigen::Map<Eigen::MatrixXd> precLambda_map() { return Eigen::Map<Eigen::MatrixXd>(precLambda_ptr, precLambda_rows, precLambda_cols); }
 
     AllReduceBlock(
+        unsigned int sum_size,
+        unsigned int cov_size,
+        unsigned int precMu_size,
+        unsigned int precMu_rows,
+        unsigned int precMu_cols,
+        unsigned int precLambda_size,
+        unsigned int precLambda_rows,
+        unsigned int precLambda_cols
+    ) : all_data(sum_size + cov_size + 1 + precMu_size + precLambda_size)
+    {
+        double *p = all_data.data();
+        sum_ptr = p;
+        p += sum_size;
+
+        cov_ptr = p;
+        p += cov_size;
+
+        norm_ptr = p;
+        p += 1;
+
+        precMu_ptr = p;
+        p += precMu_size;
+        this->precMu_rows = precMu_rows;
+        this->precMu_cols = precMu_cols;
+
+        precLambda_ptr = p;
+        p += precLambda_size;
+        this->precLambda_rows = precLambda_rows;
+        this->precLambda_cols = precLambda_cols;
+
+        assert((p - all_data.data()) == all_data.size());
+        sum_map().setZero();
+        cov_map().setZero();
+        norm_ref() = .0;
+        precMu_map().setZero();
+        precLambda_map().setZero();
+    }
+
+    AllReduceBlock(
         const VectorNd &sum,  //-- sum of all U-vectors
         const MatrixNNd &cov, //-- covariance
         double norm,
@@ -95,22 +134,32 @@ void MPI_Sys::sample(Sys &in)
         BPMF_COUNTER("allreduce");
         blocks.push_back(AllReduceBlock(sum, cov, norm, precMu, precLambda));
         const unsigned num_blocks = blocks.size();
-        const unsigned slack = 4;
+        const unsigned slack = 16;
 
-        std::default_random_engine generator;
-        std::uniform_int_distribution<int> distribution(0,slack);
+        static std::default_random_engine generator;
+        static std::uniform_int_distribution<int> distribution(0,slack);
         const unsigned random_slack = distribution(generator);
 
         auto &block = (num_blocks > slack) ? blocks.at(num_blocks-1-random_slack) : blocks.back();
         Sys::cout() << "Using slack " << random_slack << std::endl;
 
-        MPI_Allreduce(MPI_IN_PLACE, block.all_data.data(), block.all_data.size(), MPI_DOUBLE, MPI_SUM, MPI_COMM_WORLD);
+        AllReduceBlock new_block(
+            sum.nonZeros(),
+            cov.nonZeros(),
+            precMu.nonZeros(),
+            precMu.rows(),
+            precMu.cols(),
+            precLambda.nonZeros(),
+            precLambda.rows(),
+            precLambda.cols()
+        );
+        MPI_Allreduce(new_block.all_data.data(), block.all_data.data(), block.all_data.size(), MPI_DOUBLE, MPI_SUM, MPI_COMM_WORLD);
 
-        sum = block.sum_map();
-        cov = block.cov_map();
-        norm = block.norm_ref();
-        precMu = block.precMu_map();
-        precLambda = block.precLambda_map();
+        sum        = new_block.sum_map();
+        cov        = new_block.cov_map();
+        norm       = new_block.norm_ref();
+        precMu     = new_block.precMu_map();
+        precLambda = new_block.precLambda_map();
 
     }
 
