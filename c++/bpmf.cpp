9899db107b1b2f845455e760089e60be1603201c
Tom Vander Aa <vanderaa@imec.be>
2021-05-06 16:01:16 +0200

    WIP: simpily - still crashing - thats good

--- c++/bpmf.cpp
+++ c++/bpmf.cpp
@@ -4,7 +4,23 @@
  */
 
 
-#include "bpmf.h"
+#define EIGEN_RUNTIME_NO_MALLOC 1
+#define EIGEN_DONT_PARALLELIZE 1
+
+#include "Eigen/Dense"
+#include "Eigen/Sparse"
+
+const int num_latent = 32;
+
+typedef Eigen::SparseMatrix<double> SparseMatrixD;
+typedef Eigen::Matrix<double, num_latent, num_latent> MatrixNNd;
+typedef Eigen::Matrix<double, num_latent, Eigen::Dynamic> MatrixNXd;
+typedef Eigen::Matrix<double, num_latent, 1> VectorNd;
+typedef Eigen::Map<MatrixNXd, Eigen::Aligned> MapNXd;
+typedef Eigen::Map<Eigen::VectorXd, Eigen::Aligned> MapXd;
+
+#pragma omp declare reduction (VectorPlus : VectorNd : omp_out.noalias() += omp_in) initializer(omp_priv = VectorNd::Zero())
+#pragma omp declare reduction (MatrixPlus : MatrixNNd : omp_out.noalias() += omp_in) initializer(omp_priv = MatrixNNd::Zero())
 
 void computeMuLambda_2lvls_standalone(long idx) 
 {
