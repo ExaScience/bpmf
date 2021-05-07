b4d88dcf0d4b2fe221ebf77a67475366e602ace9
Tom Vander Aa <vanderaa@imec.be>
2021-05-07 08:51:11 +0200

    WIP: simpily - still crashing - thats good

--- bpmf.cpp
+++ bpmf.cpp
@@ -1,35 +1,40 @@
-/*
- * Copyright (c) 2014-2016, imec
- * All rights reserved.
- */
+#include <cstdio>
 
-
-#include "Eigen/Dense"
-
-const int num_latent = 32;
-
-typedef Eigen::Matrix<double, num_latent, 1> VectorNd;
-
-#pragma omp declare reduction (VectorPlus : VectorNd : omp_out.noalias() += omp_in) initializer(omp_priv = VectorNd::Zero())
-
-void computeMuLambda_2lvls_standalone(long c) 
-{
-    VectorNd rr_local(VectorNd::Zero());
-
-#pragma omp taskloop default(none) shared(c) reduction(VectorPlus:rr_local)
-    for (unsigned j = 0; j < c; j++)
-    {
-        auto col = VectorNd::Zero();
+struct Array {
+    int c[10];
+    Array() {
+        for(int i = 0; i<10; i++)
+            c[i] = 0;
     }
+    void operator+=(Array b) {
+        for(int i = 0; i<10; i++) 
+            c[i] += b.c[i]; 
+    }
+
+ };
+
+#pragma omp declare reduction (ArrayPlus : Array : omp_out += omp_in)
+
+void taskloop_reduce(long c) 
+{
+    Array aa;
+
+#pragma omp taskloop default(none) shared(c) reduction(ArrayPlus: aa)
+    for (unsigned j = 0; j < c; j++)
+    { }
 
 }
 
 int main(int argc, char *argv[])
 {
+    printf("-- call with argc ?? -- \n");
+    taskloop_reduce(argc);
+    printf("-- call with argc ok -- \n");
     for(int i=0; i<10000; ++i)
     {
-        printf("%i\n", i);
-        computeMuLambda_2lvls_standalone(i);
+        printf("-- call with %d ?? -- \n", i);
+        taskloop_reduce(i);
+        printf("-- call with %d ok -- \n", i);
     }
-    printf("Success!\n");
+    printf("finished\n");
 }
